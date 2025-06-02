a8019110907300d44c2f83c4a7d0bcb9717aca88
yurahriaziev <yura.hriaziev@gmail.com>
2025-06-02 18:59:02 -0400

    course model

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean, Text, Table
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.dialects.postgresql import UUID, ARRAY
 from datetime import datetime, timezone
