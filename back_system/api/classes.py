8c5ac86d7801bc5e32b4933b5f80f438bc4ad5ce
yurahriaziev <yura.hriaziev@gmail.com>
2025-09-15 18:08:27 -0400

    progress with google auth

--- back_system/api/classes.py
+++ back_system/api/classes.py
@@ -1,6 +1,15 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, Request, Depends, HTTPException
 from fastapi.responses import RedirectResponse
-from services.google_meet_service import get_google_auth_url
+from services.google_meet_service import get_google_auth_url, fetch_google_tokens
+from sqlalchemy.orm import Session
+from db.session import SessionLocal
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 router = APIRouter()
 
@@ -11,3 +20,12 @@ def google_login(response:Response):
     response.set_cookie(key='state', value=state, httponly=True)
 
     return RedirectResponse(url=auth_url)
+
+@router.get('/auth/google/callback')
+def google_callback(request: Request, code: str, state: str, db: Session = Depends(get_db)):
+    stored_state = request.cookies.get('state')
+    if stored_state != state:
+        raise HTTPException(status_code=401, detail='Not authorized')
+    
+    tokens = fetch_google_tokens(code)
+    
--- back_system/services/google_meet_service.py
+++ back_system/services/google_meet_service.py
@@ -7,7 +7,7 @@ load_dotenv()
 SCOPES = ['https://www.googleapis.com/auth/calendar']
 CREDENTIALS = os.getenv('G_CREDENTIALS')
 
-def create_google_auth():
+def create_google_auth_flow():
     """
     create and configure the Google OAuth2.0 Flow object
     reads the credentials file and the required scopes needed and makes the redirect uri to 
@@ -23,7 +23,14 @@ def create_google_auth():
     return flow
 
 def get_google_auth_url():
-    flow = create_google_auth()
+    flow = create_google_auth_flow()
     url, state = flow.authorization_url(access_type='offline', prompt='consent')
 
     return url, state
+
+def fetch_google_tokens(code):
+    flow = create_google_auth_flow()
+
+    flow.fetch_token(code=code)
+
+    return flow.credentials
