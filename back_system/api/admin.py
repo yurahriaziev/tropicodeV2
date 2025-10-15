faa5d2dfbd7e6a3dc11b550d9027d14808658fd5
yurahriaziev <yura.hriaziev@gmail.com>
2025-10-15 16:20:54 -0400

    route to fetch server logs for admin

--- back_system/api/admin.py
+++ back_system/api/admin.py
@@ -12,6 +12,10 @@ from typing import List
 from db.session import SessionLocal
 from sqlalchemy.orm import Session
 
+import os
+
+LOG_DIR = os.path.join(os.path.dirname(__file__), "../core/logs/tropicode_api.log")
+
 router = APIRouter(prefix='/admin')
 
 def get_db():
@@ -34,6 +38,18 @@ def get_activity(admin: User = Depends(get_admin_user), db: Session = Depends(ge
     activities = db.query(AdminActivity).order_by(AdminActivity.timestamp.desc())
     return activities
     
+@router.get('/logs', tags=['Admin'])
+def get_logs(admin: User = Depends(get_admin_user)):
+    try:
+        if not os.path.exists(LOG_DIR):
+            raise HTTPException(status_code=404, detail='Log file not found')
+        
+        with open(LOG_DIR, 'r') as file:
+            lines = file.readlines()[-100:]
+
+        return {'lines':[line.strip() for line in lines]}
+    except Exception as e:
+        logger.info(f'[ADMIN]user_id={admin.id} | route=/api/admin/logs | error=Failed to read logs: {e}')
 
 @router.post('/messages', tags=['Admin'])
 def send_messages(admin: User = Depends(get_admin_user), db: Session = Depends(get_db)):
@@ -45,8 +61,4 @@ def create_user(user:UserCreate, admin: User = Depends(get_admin_user), db: Sess
 
 @router.post('/onboarding-link', tags=['Admin'])
 def generate_onboarding_link(admin: User = Depends(get_admin_user)):
-    pass
-
-@router.get('/logs', tags=['Admin'])
-def get_logs(admin: User = Depends(get_admin_user)):
     pass
